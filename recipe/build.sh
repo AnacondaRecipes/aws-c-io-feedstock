9d3b063d665e6d46ac1baa512842d46895428c88
PatrykKups <pkups@anaconda.com>
2025-06-27 13:57:09 +0200

    test

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,11 +16,31 @@ cmake ${CMAKE_ARGS} -GNinja \
 
 cmake --build . --config Release --target install
 
-# To fix tests on Mac
 if [[ ${target_platform} == osx-* ]]; then
-  export DYLD_LIBRARY_PATH=$PREFIX/lib/libaws-c-io${SHLIB_EXT}
-fi
+  # Following tests demands provide password to root.
+  EXCLUDE_ROOT_TESTS_APPLE="\
+  test_concurrent_cert_import|\
+  test_duplicate_cert_import|\
+  tls_channel_echo_and_backpressure_test|\
+  tls_channel_shutdown_with_cache_test|\
+  tls_channel_shutdown_with_cache_window_update_after_shutdown_test|\
+  tls_server_multiple_connections|\
+  tls_server_hangup_during_negotiation|\
+  test_pkcs8_import|\
+  tls_channel_statistics_test|\
+  tls_certificate_chain_test"
 
-ctest --output-on-failure -j${CPU_COUNT}
+  # Failed to load shared library at path "../libaws-c-io.so" <- wrong dynamic lib format on macos hardcoded in test
+  # https://github.com/awslabs/aws-c-io/blob/ee7925a345c336b9a9c2f6843422297c5f2a7b0f/tests/shared_library_test.c#L16
+  EXCLUDE_DYLIB_TESTS_APPLE="\
+  shared_library_open_success|\
+  shared_library_find_function_failure|\
+  shared_library_find_function_success"
+
+  EXCLUDE_TEST_APPLE="${EXCLUDE_ROOT_TESTS_APPLE}|${EXCLUDE_DYLIB_TESTS_APPLE}"
+  ctest -E "$EXCLUDE_TEST_APPLE" --output-on-failure -j${CPU_COUNT}
+else
+  ctest --output-on-failure -j${CPU_COUNT}
+fi
 
 popd
