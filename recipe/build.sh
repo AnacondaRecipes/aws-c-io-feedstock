c1027fa998696da2a8626875575918937fd38579
PatrykKups <pkups@anaconda.com>
2025-06-27 13:30:30 +0200

    test

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,7 @@ cmake ${CMAKE_ARGS} -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=ON \
   -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-  -DAWS_USE_APPLE_NETWORK_FRAMEWORK=ON \
+  -DDYLD_LIBRARY_PATH=$PREFIX/lib:$DYLD_LIBRARY_PATH
   ..
 
 cmake --build . --config Release --target install
