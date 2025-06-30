8f3cc069657c257144717f044eb577523575809a
PatrykKups <pkups@anaconda.com>
2025-06-30 11:13:33 +0200

    aws-c-io 0.17.0 rebuild (#3)
    * KG-8575: aws-c-io 0.17.0 rebuild

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,19 @@
 mkdir "%SRC_DIR%"\build
 pushd "%SRC_DIR%"\build
 
-cmake -G "Ninja" ^
+cmake -GNinja ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -DCMAKE_INSTALL_LIBDIR=lib ^
       -DCMAKE_BUILD_TYPE=Release ^
+      -DENABLE_TESTING=ON ^
       -DBUILD_SHARED_LIBS=ON ^
+      -DCMAKE_POSITION_INDEPENDENT_CODE=ON ^
       ..
 if errorlevel 1 exit 1
 
-ninja install
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
+
+ctest --output-on-failure
 if errorlevel 1 exit 1
