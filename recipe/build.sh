19b1518d1c4fcfbdc7bac6e12c5678af6e113643
PatrykKups <pkups@anaconda.com>
2025-07-03 13:45:40 +0200

    aws-c-io 0.20.1  (#4)

--- recipe/build.sh
+++ recipe/build.sh
@@ -26,10 +26,11 @@ tls_channel_shutdown_with_cache_test|\
 tls_channel_shutdown_with_cache_window_update_after_shutdown_test|\
 tls_server_multiple_connections|\
 tls_server_hangup_during_negotiation|\
+tls_client_channel_negotiation_error_socket_closed|\
 test_pkcs8_import|\
 tls_channel_statistics_test|\
 tls_certificate_chain_test"
-  
+
   # Test fails to load shared library at "../libaws-c-io.so" due to incompatible dynamic library format on macOS (hardcoded in test).
   # https://github.com/awslabs/aws-c-io/blob/ee7925a345c336b9a9c2f6843422297c5f2a7b0f/tests/shared_library_test.c#L16
   EXCLUDE_DYLIB_TESTS_APPLE="\
@@ -38,7 +39,7 @@ shared_library_find_function_failure|\
 shared_library_find_function_success"
 
   EXCLUDE_TEST_APPLE="${EXCLUDE_ROOT_TESTS_APPLE}|${EXCLUDE_DYLIB_TESTS_APPLE}"
-  ctest -E "$EXCLUDE_TEST_APPLE" --output-on-failure -j${CPU_COUNT}
+  ctest -E "${EXCLUDE_TEST_APPLE}" --output-on-failure -j${CPU_COUNT}
 else
   ctest --output-on-failure -j${CPU_COUNT}
 fi
