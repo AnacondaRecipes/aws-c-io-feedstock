2b1524a64cc1ccc493eb1a9c4b8907f5054c2e1c
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2026-09-21 09:27:24 +0200

    aws-c-io 0.27.6  (#10)
    * version 0.27.6
    
    * add staging
    
    * fix staging url
    
    * enable s2n on windows
    
    * Revert "enable s2n on windows"
    
    This reverts commit 8f94b3af5d428d5d03e1d0e3cd08c71cae437073.
    
    * update test name in skip
    
    * Initiate build
    
    * remove staging

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -17,6 +17,6 @@ if errorlevel 1 exit 1
 
 REM Expected condition to be false: outgoing_args.error_invoked
 REM Bundled test certs (tests/resources/*.crt) expired 2026-08-06; same tests already excluded on linux/osx.
-set "EXCLUDE_TESTS_WIN=tls_client_channel_negotiation_success_mtls_tls1_3|incoming_tcp_sock_errors|tls_channel_echo_and_backpressure_test|tls_channel_shutdown_with_cache_test|tls_channel_shutdown_with_cache_window_update_after_shutdown_test|tls_server_multiple_connections|tls_channel_statistics_test|tls_certificate_chain_test"
+set "EXCLUDE_TESTS_WIN=tls_client_channel_negotiation_success_mtls_tls13|incoming_tcp_sock_errors|tls_channel_echo_and_backpressure_test|tls_channel_shutdown_with_cache_test|tls_channel_shutdown_with_cache_window_update_after_shutdown_test|tls_server_multiple_connections|tls_channel_statistics_test|tls_certificate_chain_test"
 ctest -E "%EXCLUDE_TESTS_WIN%" --output-on-failure
 if errorlevel 1 exit 1
